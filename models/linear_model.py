c0f4640bd64d13b2b3767f5388d6ef737c824cb8
ethan-cheong <ethancheong@hotmail.sg>
2024-10-12 21:44:05 -0400

    Added equations for logistic regression sgd

--- models/linear_model.py
+++ models/linear_model.py
@@ -1,5 +1,7 @@
-# Linear models
-# Initialization automatically fits
+"""
+Implementations of linear models. Closely follows the sklearn API.
+"""
+
 from abc import ABC, abstractmethod
 import numpy as np
 
@@ -31,3 +33,25 @@ class LinearRegression(LinearModel):
   def predict(self, X):
     X = np.hstack((np.ones([X.shape[0], 1]), X))
     return X @ np.insert(self.coef_, 0, self.intercept_, axis=0)
+
+    
+class LogisticRegression(LinearModel):
+  def __init__(self):
+    self.fitted_ = False
+    self.coef_ = 0
+    self.intercept_ = 0
+  
+  # Fit using stochastic gradient descent for now. Assume y = 0 or y = 1
+  def fit(self, X, y):
+  """
+  z = w @ x + b    # logit. Note that logit(p) = sigmoid_inv(p) = ln (p / 1-p)
+  sigmoid(z) = 1 / (1 + exp(-z))
+  P(y = 1) = sigmoid(z)    
+  P(y = 0) = 1 - sigmoid(z) = sigmoid(-z)
+  cross entropy loss: -1/n \sum_{i=1}^m y_i log(hat y) + (1-y_i) ln (1-y_i)
+
+  Gradients: wrt w: 1/n (z - y) \cdot x
+            wrt b:  1/n (z - y)
+  """
+
+
